f54cbad5717849fefc3d515f7eaea0a092fdbcf4
Денис Ахметов <denisahmetov@Mac-mini-Denis.local>
2023-05-01 01:22:31 +0600

    for github 1

--- blog_mele/settings.py
+++ blog_mele/settings.py
@@ -19,7 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 load_dotenv()
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -41,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main.apps.MainConfig',
 ]
 
 MIDDLEWARE = [
