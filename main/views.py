0c8dd0bb9ed913a6de7eaa4a7c14bef859cd0436
Денис Ахметов <denisahmetov@Mac-mini-Denis.local>
2023-05-13 08:34:36 +0600

    Chapter1

--- main/views.py
+++ main/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+# from django.http import Http404
+from django.contrib.auth import get_user_model
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
-# Create your views here.
+User = get_user_model()
+
+
+def post_list(request):
+    posts = Post.published.all()
+    return render(request, 'main/post/list.html', {'posts': posts})
+
+
+def post_detail(request, id):
+    # try:
+    #     post = Post.objects.get(id=id)
+    # except Post.DoesNotExist:
+    #     raise Http404
+    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+    context = {
+        'post': post,
+    }
+    return render(request, 'main/post/detail.html', context)
